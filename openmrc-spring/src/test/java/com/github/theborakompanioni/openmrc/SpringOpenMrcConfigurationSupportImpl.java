165ffc3be44f36d806ee59975665c9220a4b9caa
theborakompanioni <theborakompanioni+github@gmail.com>
2016-11-09 22:48:29 +0100

    add module openmrc-spring

---
+++ openmrc-spring/src/test/java/com/github/theborakompanioni/openmrc/SpringOpenMrcConfigurationSupportImpl.java
@@ -0,0 +1,4 @@
+package com.github.theborakompanioni.openmrc;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
---
+++ openmrc-spring/src/test/java/com/github/theborakompanioni/openmrc/SpringOpenMrcConfigurationSupportTestIT.java
@@ -0,0 +1,77 @@
+package com.github.theborakompanioni.openmrc;
+
+import com.codahale.metrics.MetricRegistry;
+import com.github.theborakompanioni.openmrc.mapper.OpenMrcHttpRequestMapper;
+import com.github.theborakompanioni.openmrc.web.OpenMrcHttpRequestService;
+import com.google.protobuf.ExtensionRegistry;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.Collections;
+import java.util.List;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.notNullValue;
+import static org.junit.Assert.assertThat;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest(
+        classes = SpringOpenMrcConfigurationSupportTestIT.SpringOpenMrcConfigurationSupportImpl.class
+)
+public class SpringOpenMrcConfigurationSupportTestIT {
+
+    @SpringBootApplication
+    public static class SpringOpenMrcConfigurationSupportImpl extends SpringOpenMrcConfigurationSupport {
+    }
+    
+    @Autowired
+    public ApplicationContext context;
+
+    @Autowired(required = false)
+    private List<OpenMrcRequestConsumer> requestConsumers = Collections.emptyList();
+
+    @Autowired(required = false)
+    private List<OpenMrcRequestInterceptor<HttpServletRequest>> requestInterceptors = Collections.emptyList();
+
+    @Test
+    public void metricsRegistry() throws Exception {
+        final MetricRegistry bean = context.getBean(MetricRegistry.class);
+        assertThat(bean, is(notNullValue()));
+    }
+
+    @Test
+    public void extensionRegistry() throws Exception {
+        final ExtensionRegistry bean = context.getBean(ExtensionRegistry.class);
+        assertThat(bean, is(notNullValue()));
+    }
+
+    @Test
+    public void httpRequestMapper() throws Exception {
+        final OpenMrcHttpRequestMapper bean = context.getBean(OpenMrcHttpRequestMapper.class);
+        assertThat(bean, is(notNullValue()));
+    }
+
+    @Test
+    public void httpRequestService() throws Exception {
+        final OpenMrcHttpRequestService bean = context.getBean(OpenMrcHttpRequestService.class);
+        assertThat(bean, is(notNullValue()));
+    }
+
+    @Test
+    public void openMrcRequestConsumer() throws Exception {
+        assertThat(requestConsumers, is(notNullValue()));
+        assertThat(requestConsumers.isEmpty(), is(false));
+    }
+
+    @Test
+    public void httpRequestInterceptor() throws Exception {
+        assertThat(requestInterceptors, is(notNullValue()));
+        assertThat(requestInterceptors.isEmpty(), is(false));
+    }
+}
